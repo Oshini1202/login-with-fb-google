7bf479611ab90f6a306e203fb8437750b74cc311
Oshini1202 <aishcharya.2019757@iit.ac.lk>
2021-04-20 15:59:20 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {GoogleLoginProvider, SocialLoginModule} from 'angularx-social-login';
+import { FacebookLoginProvider } from "angularx-social-login";
+import {AuthGuardService} from './auth-guard.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'mainpage', component: MainPageComponent, canActivate: [AuthGuardService]},
+      {path: '**', component: LoginComponent}
+    ]),
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('280443159982-2pt3b22s9gqm2v749nvcct18es6h5r31.apps.googleusercontent.com')
+        },
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider("469941280795860")
+        }
+      ]
+    }
+  },
+    AuthGuardService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+import {GoogleLoginProvider, SocialAuthService} from 'angularx-social-login';
+import { FacebookLoginProvider} from "angularx-social-login";
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private router: Router,
+              private socialAuthService: SocialAuthService) {
+  }
+
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
+      .then(() => this.router.navigate(['mainpage']));
+  }
+  signInWithFB(): void {
+    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID)
+    .then(() => this.router.navigate(['mainpage']));
+  
+  } 
+}
